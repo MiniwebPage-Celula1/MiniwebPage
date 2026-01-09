129f1f71a60180f618546de8688cc53880d7cac5
Cayuro <juan_gomez2000@hotmail.com>
2026-01-08 21:22:24 -0500

    botones funcionales

--- JS/home.js
+++ JS/home.js
@@ -4,21 +4,45 @@ const exitButton = document.getElementById('submitExit');
 const backgroundForm = document.getElementById('backgroundForm');
 const filterTaskCard = document.getElementById('filterTaskCard');
 const filterTaskButton = document.getElementById('filterTaskButton');
+const alertDeleteAll = document.getElementById('alertDeleteAllCard');
+const deleteAllButton = document.getElementById('deleteAllButton');
+const noButton = document.getElementById('noButton');
 
+function closeAllCards() {
+    createTaskCard.style.display = 'none';
+    filterTaskCard.style.display = 'none';
+    alertDeleteAll.style.display = 'none';
+    backgroundForm.style.display = 'none';
+}
 
 addTaskButton.addEventListener('click', () => {
+    closeAllCards();
     createTaskCard.style.display = 'block';
-        backgroundForm.style.display = 'block';
-        
+    backgroundForm.style.display = 'block';
 });
 
-filterTaskButton.addEventListener('click', () =>{
+filterTaskButton.addEventListener('click', () => {
+    closeAllCards();
     filterTaskCard.style.display = 'block';
+    backgroundForm.style.display = 'block';
+});
+
+deleteAllButton.addEventListener('click', () =>{
+    closeAllCards();
+    alertDeleteAll.style.display = 'block';
+    backgroundForm.style.display = 'block';
 })
 exitButton.addEventListener('click', () => {
-    createTaskCard.style.display = 'none';
-    backgroundForm.style.display = 'none';
+    closeAllCards();
 });
+noButton.addEventListener('click', () =>{
+    closeAllCards();
+})
+backgroundForm.addEventListener('click', () => {
+    closeAllCards();
+});
+
+
 
 
 
