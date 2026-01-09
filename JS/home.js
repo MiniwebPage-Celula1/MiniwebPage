4e0b44c33639c5a068123917983636deb5dfa674
Tomy20050211 <tomasruiz1104@gmail.com>
2026-01-09 16:39:08 -0500

    alertas listas

--- JS/home.js
+++ JS/home.js
@@ -1,20 +1,38 @@
+// BOTONES PRINCIPALES
 const addTaskButton = document.getElementById('addTaskButton');
-const createTaskCard = document.getElementById('createTaskCard');
-const exitButton = document.getElementById('submitExit');
-const backgroundForm = document.getElementById('backgroundForm');
-const filterTaskCard = document.getElementById('filterTaskCard');
 const filterTaskButton = document.getElementById('filterTaskButton');
-const alertDeleteAll = document.getElementById('alertDeleteAllCard');
 const deleteAllButton = document.getElementById('deleteAllButton');
-const noButton = document.getElementById('noButton');
-const alertTaskProcess = document.getElementById('alertTaskProcess')
+const deleteTaskButton = document.getElementById('deleteTaskButton');
+// FORMULARIOS / TARJETAS
+const createTaskCard = document.getElementById('createTaskCard');
+const filterTaskCard = document.getElementById('filterTaskCard');
+
+// BOTONES DE CIERRE / EXIT
+const exitButton = document.getElementById('buttontExitTask');
+const exitFilterButton = document.getElementById('exitFilterButton');
+const backgroundForm = document.getElementById('backgroundForm');
+
+// ALERTAS / CONFIRMACIONES
+const alertDeleteAll = document.getElementById('alertDeleteAllCard');
+const alertTaskProcess = document.getElementById('alertTaskProcess');
+const alertTaskDelete = document.getElementById('alertTaskDelete');
+
+// BOTONES DE CONFIRMACIÓN / CANCELACIÓN
 const yesTaskButton = document.getElementById('yesTaskButton');
 const noTaskButton = document.getElementById('noTaskButton');
+const yesTaskDeleteButton = document.getElementById('yesTaskDeleteButton');
+const noTaskDeleteButton = document.getElementById('noTaskDeleteButton');
+const noButton = document.getElementById('noButton');
+
+
+
 
 function closeAllCards() {
     createTaskCard.style.display = 'none';
     filterTaskCard.style.display = 'none';
     alertDeleteAll.style.display = 'none';
+    alertTaskProcess.style.display = 'none';
+    alertTaskDelete.style.display = 'none';
     backgroundForm.style.display = 'none';
 }
 
@@ -35,9 +53,12 @@ deleteAllButton.addEventListener('click', () =>{
     alertDeleteAll.style.display = 'block';
     backgroundForm.style.display = 'block';
 })
-exitButton.addEventListener('click', () => {
-    closeAllCards();
+exitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    alertTaskProcess.style.display = 'block';
+
 });
+
 noButton.addEventListener('click', () =>{
     closeAllCards();
 })
@@ -45,8 +66,24 @@ backgroundForm.addEventListener('click', () => {
     closeAllCards();
 });
 
+noTaskButton.addEventListener('click', () => {
+    alertTaskProcess.style.display = 'none';
+    createTaskCard.style.display = 'block';
+    backgroundForm.style.display = 'block';
+    console.log(noTaskButton)
+});
+
+yesTaskButton.addEventListener('click', () =>{
+    closeAllCards();
+    console.log(yesTaskButton)
+})
+
+deleteTaskButton.addEventListener('click', () =>{
+    alertTaskDelete.style.display = 'block'
+    backgroundForm.style.display = 'block'
+})
 
 
-
-
-
+noTaskDeleteButton.addEventListener('click', () =>{
+    closeAllCards();
+})
